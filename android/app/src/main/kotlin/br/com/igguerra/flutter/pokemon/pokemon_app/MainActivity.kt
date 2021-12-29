a0d2115203e631d9739143fced15c1659b5f991d
Igor Guerra <igguerra07@gmail.com>
2021-12-29 09:15:39 -0400

    :tada:  initial commit

---
+++ android/app/src/main/kotlin/br/com/igguerra/flutter/pokemon/pokemon_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.igguerra.flutter.pokemon.pokemon_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_app/app/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
