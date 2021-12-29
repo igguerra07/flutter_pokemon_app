bba47c8ee90c4360082929e18bdb754d1c55af6e
Igor Guerra <igguerra07@gmail.com>
2021-12-29 12:00:23 -0400

    :art: add dependency injection implements

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pokemon_app/app/app_widget.dart';
+import 'package:pokemon_app/app/di/injector.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await configureDependencies();
+
   runApp(const AppWidget());
 }
 
