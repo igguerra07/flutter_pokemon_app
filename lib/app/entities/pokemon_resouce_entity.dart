2d1ce76e6bb1c2306880668d4c35d49bcac9610b
Igor Guerra <igguerra07@gmail.com>
2022-01-16 11:01:11 -0400

    :truck: change pokemon entity to pokemon resource entity

--- lib/app/entities/pokemon_resouce_entity.dart
+++ lib/app/entities/pokemon_resouce_entity.dart
@@ -1,12 +1,11 @@
 import 'package:equatable/equatable.dart';
 import 'package:pokemon_app/services/api/pokemon/pokemon_service.dart';
 
-
-class PokemonEntity extends Equatable {
+class PokemonResourceEntity extends Equatable {
   final String name;
   final String url;
 
-  const PokemonEntity({
+  const PokemonResourceEntity({
     required this.url,
     required this.name,
   });
